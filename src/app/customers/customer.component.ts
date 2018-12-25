535aac00682e5e165846ae3c56fce15af3124b25
Jeffery Cosby <jcoz1968@hotmail.com>
2018-12-25 17:13:04 -0600

    formarray for addresses

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn, FormArray } from '@angular/forms';
 import { debounceTime } from 'rxjs/operators';
 
 import { Customer } from './customer';
@@ -36,6 +36,10 @@ export class CustomerComponent implements OnInit {
   customer = new Customer();
   emailMessage: string;
 
+  get addresses(): FormArray {
+    return <FormArray>this.customerForm.get('addresses');
+  }
+
   private validationMessages = {
     required: 'Please enter your email address.',
     email: 'Please enter a fvalid email address.'
@@ -55,7 +59,7 @@ export class CustomerComponent implements OnInit {
       notification: 'email',
       rating: [ null, ratingRange(1, 5) ],
       sendCatalog: false,
-      addresses: this.buildAddress()
+      addresses: this.fb.array([ this.buildAddress() ])
     });
 
     this.customerForm.get('notification').valueChanges.subscribe(val => {
