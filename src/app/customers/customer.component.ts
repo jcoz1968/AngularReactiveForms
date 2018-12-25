9c7fe7de4906acd33de916afcb779c3372396f11
Jeffery Cosby <jcoz1968@hotmail.com>
2018-12-25 15:24:32 -0600

    set validators at runtime

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import { Customer } from './customer';
 
@@ -16,9 +16,11 @@ export class CustomerComponent implements OnInit {
 
   ngOnInit() {
     this.customerForm = this.fb.group({
-      firstName: '',
-      lastName: '',
-      email: '',
+      firstName: ['', [ Validators.required, Validators.minLength(3)] ],
+      lastName: ['', [ Validators.required, Validators.maxLength(50)] ],
+      email: ['', [ Validators.required, Validators.email] ],
+      phone: ['', [ Validators.required ] ],
+      notification: 'email',
       sendCatalog: false
     });
   }
@@ -36,4 +38,16 @@ export class CustomerComponent implements OnInit {
       sendCatalog: false
     });
   }
+
+  setNotification(notifyVia: string): void {
+    const phoneControl = this.customerForm.get('phone');
+    if (notifyVia === 'text') {
+      phoneControl.setValidators(Validators.required);
+    } else {
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity();
+  }
+
+
 }
